1b296f175f1ebc2ef306f16470cda8f0b3b09e63
carozozo <carozozo@yahoo.com.tw>
2015-05-12 00:59:50 +0800

    移除 Object.copyByObjKey

--- src/js/lib/object.js
+++ src/js/lib/object.js
@@ -150,32 +150,28 @@
    * @param {object} obj
    * @param {string[]|string} keys the element that want to copy by keys
    * @param {object} [opt]
-   * @param {boolean} [opt.clone=true] if clone for not replacing original
+   * @param {boolean} [opt.clone=false] if clone for not replacing original
    * @param {boolean} [opt.keep=true] if keep original element
    * @returns {{}}
    */
-  self.copyByObjKey = function(obj, keys, opt) {
-    var clone, keep, obj2;
-    clone = true;
-    keep = true;
-    obj2 = {};
+  self.getByObjKey = function(obj, keys, opt) {
+    var clone, keep, r;
+    r = null;
     keys = caro.splitStr(keys, ',');
-    opt = caro.isObj(opt) ? opt : {};
-    if (opt) {
-      clone = opt.clone !== false;
-      keep = opt.keep !== false;
-    }
+    opt = caro.coverToObj(opt);
+    clone = opt.clone === true;
+    keep = opt.keep !== false;
     caro.eachObj(keys, function(i, key) {
       if (clone) {
-        obj2[key] = caro.cloneObj(obj[key]);
+        r = caro.cloneObj(obj[key]);
       } else {
-        obj2[key] = obj[key];
+        r = obj[key];
       }
       if (!keep) {
         delete obj[key];
       }
     });
-    return obj2;
+    return r;
   };
 
   /**
