a554130f41d563bc00e8e7cad7daa65f65315905
carozozo <carozozo@yahoo.com.tw>
2015-05-16 14:14:10 +0800

    v0.4.12

--- js/lib/object.js
+++ js/lib/object.js
@@ -59,16 +59,6 @@
   };
 
   /**
-   * TODO will replaced by Loop.each
-   * like jQuery.each function
-   * @param {object} obj
-   * @param {function} cb callback-fn for each key & val
-   */
-  self.eachObj = function(obj, cb) {
-    return caro.each(obj, cb);
-  };
-
-  /**
    * @param {object} obj
    * @returns {Number}
    */
