28be996acf855e05d37c314e2a3e45a7f95f8646
Caro.Huang <carozozo@yahoo.com.tw>
2015-05-23 17:31:28 +0800

    bind lodash

--- caro.js
+++ caro.js
@@ -1,12 +1,13 @@
 /*! caro - v0.5.15 - 2015-05-23 */
 (function(g) {
   var caro;
-  caro = {};
+  caro = typeof _ !== "undefined" && _ !== null ? _ : {};
   caro.isNode = (function() {
     return (typeof global !== "undefined" && global !== null) && (typeof module !== "undefined" && module !== null) && (typeof exports !== "undefined" && exports !== null);
   })();
   g.caro = caro;
   if (caro.isNode) {
+    caro = require('lodash');
     module.exports = caro;
     return global.caro = caro;
   }
@@ -343,7 +344,7 @@
  */
 (function() {
   var coverFormatType, coverLocale, defLocale, getDateTimeObj, nMoment, oShorthandFormat, returnDateTimeStr, self;
-  nMoment = caro.isNode ? require('moment') : moment;
+  nMoment = caro.isNode ? require('moment') : typeof moment !== "undefined" && moment !== null ? moment : null;
   if (nMoment == null) {
     return;
   }
--- coffee/caro.coffee
+++ coffee/caro.coffee
@@ -1,8 +1,10 @@
 ((g) ->
-  caro = {}
+  caro = if _? then _ else {}
   caro.isNode = do ->
     return global? and module? and exports?
   g.caro = caro
   if caro.isNode
+    caro = require 'lodash'
     module.exports = caro
-    global.caro = caro) this
+    global.caro = caro
+) this
