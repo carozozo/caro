0584eb0869dce3e649f7a5520a1002b17e17e9da
Caro.Huang <carozozo@yahoo.com.tw>
2015-05-24 22:56:33 +0800

    remove Object ownnKey/getKeysInObj

--- caro.js
+++ caro.js
@@ -1,4 +1,4 @@
-/*! caro - v0.6.19 - 2015-05-24 */
+/*! caro - v0.6.20 - 2015-05-24 */
 (function(g) {
   var caro, isNode;
   caro = typeof _ !== "undefined" && _ !== null ? _ : {};
@@ -270,56 +270,6 @@
 (function() {
   var self;
   self = caro;
-
-  /**
-   * check if key exists in object, will return false when key not exist, no matter that other-keys are
-   * @param {object} obj
-   * @param {string} keys the keys that want to validate
-   * @returns {boolean}
-   */
-  self.ownKey = function(obj, key) {
-    var aKey, pass;
-    pass = true;
-    aKey = caro.drop(arguments);
-    caro.forEach(aKey, function(key) {
-      if (!obj.hasOwnProperty(key)) {
-        pass = false;
-        return false;
-      }
-    });
-    return pass;
-  };
-
-  /**
-   * get keys in object, and get all if levelLimit = 0
-   * @param {object} obj
-   * @param {number} [levelLimit=1] the level of object you want to get keys
-   * @returns {Array}
-   */
-  self.getKeysInObj = function(obj, levelLimit) {
-    var getKey, levelCount, r;
-    r = [];
-    if (!caro.isObject(obj)) {
-      return r;
-    }
-    levelLimit = caro.coverToInt(levelLimit, false) > -1 ? levelLimit : 1;
-    levelCount = 0;
-    getKey = function(obj) {
-      levelCount++;
-      caro.forEach(obj, function(val, key) {
-        if (levelLimit > 0 && levelCount > levelLimit) {
-          return;
-        }
-        r.push(key);
-        if (caro.isObject(val)) {
-          getKey(val);
-        }
-      });
-      levelCount--;
-    };
-    getKey(obj);
-    return r;
-  };
 
   /**
    * covert to string if element is function in object
