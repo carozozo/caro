c26ebe802003b2ad2958723624c27c1ce78dac47
Caro.Huang <carozozo@yahoo.com.tw>
2015-05-29 23:46:51 +0800

    update Object.toWord

--- js/lib/object.js
+++ js/lib/object.js
@@ -8,29 +8,30 @@
   self = caro;
 
   /**
-   * show object/array by string
+   * display object/array by string
    * @param {object} obj
+   * @param {number} spaceLength the space before each line
    */
-  self.toWord = function(arg) {
+  self.toWord = function(arg, spaceLength) {
     var toWord;
-    toWord = function(arg, spaceLength) {
-      var addSpace, reg, ret, space;
-      spaceLength = spaceLength || 0;
-      ret = '';
-      spaceLength += 2;
-      space = '    ';
-      addSpace = caro.repeat(' ', spaceLength);
-      space += addSpace;
+    toWord = function(arg, spaceLength, layer) {
+      var fnSpace, fnSpaceLength, reg, ret, space;
+      spaceLength = spaceLength || 2;
+      layer = layer || 0;
+      fnSpaceLength = layer * 2 + 6;
+      space = caro.repeat(' ', spaceLength);
+      fnSpace = caro.repeat(' ', fnSpaceLength);
+      layer++;
       try {
         ret = JSON.stringify(arg, function(key, val) {
           if (!key) {
             return val;
           }
-          return toWord(val, spaceLength);
-        }, 2);
-        ret = ret.replace(/\\r\\n/g, '\r\n  ');
-        ret = ret.replace(/\\r/g, '\r  ');
-        ret = ret.replace(/\\n/g, '\n  ');
+          return toWord(val, spaceLength, layer);
+        }, spaceLength);
+        ret = ret.replace(/\\r\\n/g, '\r\n' + space);
+        ret = ret.replace(/\\r/g, '\r' + space);
+        ret = ret.replace(/\\n/g, '\n' + space);
         ret = ret.replace(/"/g, '');
       } catch (_error) {}
       if (ret) {
@@ -38,16 +39,18 @@
       }
       try {
         ret = arg.toString();
-        reg = new RegExp('\r\n' + space, 'g');
-        ret = ret.replace(reg, '\r\n');
-        reg = new RegExp('\r' + space, 'g');
-        ret = ret.replace(reg, '\r');
-        reg = new RegExp('\n' + space, 'g');
-        ret = ret.replace(reg, '\n');
-        ret = ret.replace(/"/g, '');
+        if (caro.isFunction(arg)) {
+          reg = new RegExp('\r\n' + fnSpace, 'g');
+          ret = ret.replace(reg, '\r\n');
+          reg = new RegExp('\r' + fnSpace, 'g');
+          ret = ret.replace(reg, '\r');
+          reg = new RegExp('\\n' + fnSpace, 'g');
+          ret = ret.replace(reg, '\n');
+          ret = ret.replace(/"/g, '');
+        }
       } catch (_error) {}
       return ret;
     };
-    return toWord(arg, 0);
+    return toWord(arg, spaceLength);
   };
 })();
--- test/unit_test.coffee
+++ test/unit_test.coffee
@@ -1,11 +1,17 @@
 #do ->
 #describe.only 'Unit', ->
 #  it 'test', ->
-#    arr = [
-#      caro.toString(['caro', undefined])
-#    ]
-#
-#    caro.forEach(arr, (val)->
-#      console.log val
-#      console.log '-----------'
-#    )
+#    a = {
+#      a: false, b: null, c: 0, d: 'caro', e: undefined, f: [3, 4, 5, 6],
+#      g: () ->
+#        return 1
+#      h: {
+#        i: 3
+#        j: () -> return 2
+#        k: {
+#          l: () -> return 3
+#        }
+#      }
+#    }
+#    r = caro.toWord(a, 6)
+#    console.log r
