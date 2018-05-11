577c172bafd753861c0677ac1ea02f98aeb4fbd4
Caro Huang <s364362@sinyi.com.tw>
2018-05-11 16:52:05 +0800

    refine Object

--- test/object.test.js
+++ test/object.test.js
@@ -1,20 +1,20 @@
-describe('Object', function() {
-  var caro, should;
-  should = require('chai').should();
-  caro = require('../dist/caro.js');
-  it('assignByKeys', function() {
-    var obj, obj2;
-    obj = {
+describe('Object', function () {
+  var should = require('chai').should();
+  var caro = require('../dist/caro.js');
+
+  it('assignByKeys', function () {
+    var obj = {
       a: 1,
       b: 2,
       c: 3,
       d: 4
     };
-    obj2 = {
+    var obj2 = {
       d: 1,
       e: 2,
       f: 3
     };
+
     caro.assignByKeys(obj, obj2, ['d', 'f']);
     obj.should.eql({
       a: 1,
@@ -29,8 +29,9 @@ describe('Object', function() {
       c: 3,
       d: 4
     };
+
     caro.assignByKeys(obj, obj2, ['d', 'f'], false);
-    return obj.should.eql({
+    obj.should.eql({
       a: 1,
       b: 2,
       c: 3,
@@ -38,100 +39,100 @@ describe('Object', function() {
       f: 3
     });
   });
-  it('catching', function() {
-    var obj, obj2, r, r2;
-    obj = {
+  it('catching', function () {
+    var obj = {
       name: 'Caro',
       age: 18
     };
-    obj2 = {
+    var obj2 = {
       country: 'Taiwan',
       city: 'Kaohsiung'
     };
-    r = {
+    var r = {
       name: ''
     };
-    r2 = {
+    var r2 = {
       name: '',
       city: ''
     };
     caro.catching(r, obj);
     caro.catching(r2, obj, obj2);
+
     r.name.should.eq('Caro');
     r2.name.should.eq('Caro');
-    return r2.city.should.eq('Kaohsiung');
+    r2.city.should.eq('Kaohsiung');
   });
-  it('classify', function() {
-    var keysArr, r, r2;
-    r = caro.classify({
+  it('classify', function () {
+    var r = caro.classify({
       a: 1,
       b: 'd',
       c: {
         cc: 1
       },
-      d: function() {},
+      d: function () {},
       e: ['caro'],
       f: 'FF'
     });
-    r2 = caro.classify([
-      'caro', '4', 3, function() {
+    var r2 = caro.classify([
+      'caro', '4', 3, function () {
         return 'a';
       }, ['m']
     ]);
-    keysArr = ['bool', 'str', 'num', 'arr', 'obj', 'fn'];
+    var keysArr = ['bool', 'str', 'num', 'arr', 'obj', 'fn'];
+
     r.should.has.keys(keysArr);
-    return r2.should.has.keys(keysArr);
+    r2.should.has.keys(keysArr);
   });
-  it('differentKeys', function() {
-    var obj, obj2, r, r2;
-    obj = {
+  it('differentKeys', function () {
+    var obj = {
       name: 'Caro',
       age: 18
     };
-    obj2 = {
+    var obj2 = {
       name: 'Snoopy',
       country: 'Taiwan',
       city: 'Kaohsiung'
     };
-    r = caro.differentKeys(obj, obj2);
-    r2 = caro.differentKeys(obj, obj2, true);
+    var r = caro.differentKeys(obj, obj2);
+    var r2 = caro.differentKeys(obj, obj2, true);
+
     r.should.eql(['age']);
-    return r2.should.eql(['country', 'city']);
+    r2.should.eql(['country', 'city']);
   });
-  it('hasEqualKeys', function() {
-    var obj, obj2, obj3, r, r2;
-    obj = {
+  it('hasEqualKeys', function () {
+    var obj = {
       name: 'Caro',
       age: 18
     };
-    obj2 = {
+    var obj2 = {
       name: 'Snoopy',
       age: 3
     };
-    obj3 = {
+    var obj3 = {
       name: 'Kitty',
       country: 'Japan'
     };
-    r = caro.hasEqualKeys(obj, obj2);
-    r2 = caro.hasEqualKeys(obj, obj3);
+    var r = caro.hasEqualKeys(obj, obj2);
+    var r2 = caro.hasEqualKeys(obj, obj3);
+
     r.should.be.true;
-    return r2.should.be.false;
+    r2.should.be.false;
   });
-  return it('sameKeys', function() {
-    var obj, obj2, r;
-    obj = {
+  it('sameKeys', function () {
+    var obj = {
       a: 1,
       b: 2,
       c: 3,
       e: 4
     };
-    obj2 = {
+    var obj2 = {
       a: 3,
       c: 4,
       d: 5,
       e: 6
     };
-    r = caro.sameKeys(obj, obj2);
-    return r.should.eql(['a', 'c', 'e']);
+    var r = caro.sameKeys(obj, obj2);
+
+    r.should.eql(['a', 'c', 'e']);
   });
 });
