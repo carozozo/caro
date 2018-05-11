73cca7b0a873bf1ee8cea3084188e07304846444
Caro Huang <s364362@sinyi.com.tw>
2018-05-11 16:22:27 +0800

    refine Loop

--- src/lib/loop.js
+++ src/lib/loop.js
@@ -2,9 +2,8 @@
  * Loop
  * @author Caro.Huang
  */
-(function() {
-  var self;
-  self = caro;
+(function () {
+  var self = caro;
   /*
    * for-loop function
    * @param {function} fn for-loop function, will break-loop when return false
@@ -12,19 +11,25 @@
    * @param {number} end
    * @param {number} step add the step in each function-called
    */
-  self.loop = function(fn, start = 0, end = 0, step = 1) {
-    var i, j, ref, ref1, ref2, res;
+  self.loop = function (fn, start, end, step) {
+    start = start ? parseInt(start, 10) : 0;
+    end = end ? parseInt(end, 10) : 0;
+    step = step ? parseInt(step, 10) : 1;
     if (start > end) {
-      step = -step;
+      step = -Math.abs(step);
     }
-    for (i = j = ref = start, ref1 = end, ref2 = step; ref2 !== 0 && (ref2 > 0 ? j <= ref1 : j >= ref1); i = j += ref2) {
-      res = fn(i);
-      if (res === false) {
-        break;
+
+    var i;
+    const compare = function () {
+      if (start < end) {
+        return i <= end;
       }
-      if (res === true) {
-        continue;
-      }
+      return i >= end;
+    };
+
+    for (i = start; compare(); i += step) {
+      var res = fn(i);
+      if (res === false) break;
     }
   };
 })();
--- test/loop.test.js
+++ test/loop.test.js
@@ -1,22 +1,22 @@
-describe('Loop', function() {
-  var caro, should;
-  should = require('chai').should();
-  caro = require('../dist/caro.js');
-  return it('loop', function() {
-    var count;
-    count = 0;
-    caro.loop(function(i) {
+describe('Loop', function () {
+  var should = require('chai').should();
+  var caro = require('../dist/caro.js');
+
+  it('loop', function () {
+    var count = 0;
+    caro.loop(function (i) {
       i.should.be.a('number');
-      return count++;
+      count++;
     }, 10, 0);
     count.should.be.eq(11);
+
     count = 0;
-    caro.loop(function(i) {
+    caro.loop(function (i) {
       var a;
       a = i % 2;
       a.should.be.eq(0);
-      return count++;
+      count++;
     }, 0, 10, 2);
-    return count.should.be.eq(6);
+    count.should.be.eq(6);
   });
 });
