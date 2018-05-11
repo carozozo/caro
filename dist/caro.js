b738309ed884a8ebbb8c4b3faa08f809d29a9257
Caro Huang <s364362@sinyi.com.tw>
2018-05-11 16:52:05 +0800

    refine Object

--- dist/caro.js
+++ dist/caro.js
@@ -446,8 +446,7 @@
  * @author Caro.Huang
  */
 (function() {
-  var self;
-  self = caro;
+  var self = caro;
   /*
    * assign elements to from obj2 to obj1 by keys
    * won't replace obj1 value if exists when argument-replace is false
@@ -456,25 +455,15 @@
    * @param {array} keys the keys in obj2 that you want sand to obj1
    * @param {boolean} [replace=true] won't replace obj1 elements if obj1 has same key when false
    */
-  self.assignByKeys = function(obj1, obj2, keys, replace = true) {
-    var j, key, len;
-    for (j = 0, len = keys.length; j < len; j++) {
-      key = keys[j];
+  self.assignByKeys = function(obj1, obj2, keys, replace) {
+    replace = replace !== undefined ? replace : true;
+
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
       if (obj2.hasOwnProperty(key) && (replace || !obj1.hasOwnProperty(key))) {
         obj1[key] = obj2[key];
       }
     }
-    //    for key of obj1
-    //      if key in obj2
-
-    //      if (key in obj2) and (replace or not key in obj1)
-    //        obj1[key] = obj2[key]
-
-    //    caro.reduce(keys, (obj, key) ->
-    //      if caro.has(obj2, key) and (replace or not caro.has(obj, key))
-    //        obj[key] = obj2[key]
-    //      return obj
-    //    , obj1)
     return obj1;
   };
   /*
@@ -483,14 +472,13 @@
    * @return {object}
    */
   self.catching = function(obj) {
-    var eachKey, eachObj, eachVal, i, j, len;
-    for (i = j = 0, len = arguments.length; j < len; i = ++j) {
-      eachObj = arguments[i];
+    for (var i = 0; i < arguments.length; i++) {
+      var eachObj = arguments[i];
       if (i === 0) {
         continue;
       }
-      for (eachKey in eachObj) {
-        eachVal = eachObj[eachKey];
+      for (var eachKey in eachObj) {
+        var eachVal = eachObj[eachKey];
         if (obj.hasOwnProperty(eachKey)) {
           obj[eachKey] = eachVal;
         }
@@ -504,15 +492,14 @@
    * @return {object}
    */
   self.classify = function(arg) {
-    var aArr, aBool, aFn, aNum, aObj, aStr, j, key, len, val;
-    aStr = [];
-    aBool = [];
-    aArr = [];
-    aNum = [];
-    aObj = [];
-    aFn = [];
-    for (val = j = 0, len = arg.length; j < len; val = ++j) {
-      key = arg[val];
+    var aStr = [];
+    var aBool = [];
+    var aArr = [];
+    var aNum = [];
+    var aObj = [];
+    var aFn = [];
+    for (var key = 0 in arg) {
+      var val = arg[key];
       if (typeof val === 'boolean') {
         aBool.push(val);
       } else if (typeof val === 'string') {
@@ -543,9 +530,10 @@
    * @return {array}
    */
   self.differentKeys = function(obj1, obj2, reverse) {
-    var difArr, j, keys1, keys2, keysA, keysB, len, val;
-    keys1 = Object.keys(obj1);
-    keys2 = Object.keys(obj2);
+    var keysA, keysB;
+    var keys1 = Object.keys(obj1);
+    var keys2 = Object.keys(obj2);
+
     if (!reverse) {
       keysA = keys1;
       keysB = keys2;
@@ -553,9 +541,10 @@
       keysA = keys2;
       keysB = keys1;
     }
-    difArr = [];
-    for (j = 0, len = keysA.length; j < len; j++) {
-      val = keysA[j];
+
+    var difArr = [];
+    for (var i = 0;  i < keysA.length; i++) {
+      var val = keysA[i];
       if (keysB.indexOf(val) < 0) {
         difArr.push(val);
       }
@@ -569,9 +558,8 @@
    * @return {boolean}
    */
   self.hasEqualKeys = function(obj1, obj2) {
-    var size1, size2;
-    size1 = caro.differentKeys(obj1, obj2).length;
-    size2 = caro.differentKeys(obj1, obj2, true).length;
+    var size1 = caro.differentKeys(obj1, obj2).length;
+    var size2 = caro.differentKeys(obj1, obj2, true).length;
     return size1 === 0 && size2 === 0;
   };
   /*
@@ -581,12 +569,11 @@
    * @return {array}
    */
   self.sameKeys = function(obj1, obj2) {
-    var diffKeys, j, keys, len, ret, val;
-    keys = Object.keys(obj1);
-    diffKeys = caro.differentKeys(obj1, obj2);
-    ret = [];
-    for (j = 0, len = keys.length; j < len; j++) {
-      val = keys[j];
+    var keys = Object.keys(obj1);
+    var diffKeys = caro.differentKeys(obj1, obj2);
+    var ret = [];
+    for (var i = 0;  i < keys.length; i++) {
+      var val = keys[i];
       if (diffKeys.indexOf(val) < 0) {
         ret.push(val);
       }
